884f754bb2ebf77669c353ac348558edcb247c56
Bairong <brongh90@gmail.com>
2023-05-12 10:14:45 +0800

    fix: nicer date time format

--- douBot/formats.go
+++ douBot/formats.go
@@ -3,6 +3,7 @@ package douBot
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	db "github.com/brongh/go-telegram-financial-bot/db"
 )
@@ -15,8 +16,14 @@ func FormatExpenses(expenses []db.Expense, total float64, month string) string {
 	var builder strings.Builder
 	builder.WriteString(fmt.Sprintf("Expenses for %s:\n=================================", month))
 	for _, expense := range expenses {
-		builder.WriteString(fmt.Sprintf("\n\nTitle: %s\nAmount: %.2f\nDate: %s\nID: %v\n\n=================================",
-		expense.Title, expense.Amount, expense.ExpenseDate, expense.Id))
+		t, err := time.Parse(time.RFC3339, expense.ExpenseDate)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return "Something went wrong. Please try again"
+		}
+
+		builder.WriteString(fmt.Sprintf("\n\nTitle: %s\nAmount: $%.2f\nDate: %s\nID: %v\n\n=================================",
+		expense.Title, expense.Amount, t.Format("2006-01-02"), expense.Id))
 	}
 	builder.WriteString(fmt.Sprintf("\n\nTotal spending in %v: $%.2f", month ,total))
 
